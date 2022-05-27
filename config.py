5cd71db6ddb8a35f3bd44576a3e1f0bffff3932c
morisdev <moris.dmitry@gmail.com>
2022-05-27 23:26:37 +0300

    add .env variables

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config:
+    NAME_DB = os.environ.get("DB_NAME")
+    USER = os.environ.get("DB_USER")
+    PASSWORD = os.environ.get("PASSWORD")
+    HOST = os.environ.get("HOST")
+    PORT = os.environ.get("PORT")
+
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME_DB}"
